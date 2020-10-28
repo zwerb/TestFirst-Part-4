ad1b3c76c9093856e91d467577580977f035b5ba
Zwerb <sy@zwerb.com>
2020-10-28 13:44:05 -0400

    Pushing files

--- 99-Zwerb/tests/program.js
+++ 99-Zwerb/tests/program.js
@@ -11,6 +11,17 @@ whatIsThis(); // this == window
 whatIsThis.call({name: 'scott'}) // this == obj {name: 'scott'}
 
 let q = ' texty '.split('<')[0];
-let r = ' textz <span> your mom </span>'.split('<')[0];
+let r = ' textz <span> teamsy boy </span>'.split('<')[0];
 
 console.log(`q: ${q} r: ${r}`)
+
+const pizza = {
+	toppings: ['Cheese', 'Ham'],
+	getToppings: function(){
+	  return "Toppings: - " + this.toppings;
+	  console.dir(this);
+	}
+  }
+  
+  
+  console.log(pizza.getToppings())
---
+++ README.md
@@ -0,0 +1,9 @@
+# TestFirst-Part-4
+
+This isn't an actual part of FSA
+
+## Using HTML, CSS, JSDOM
+
+```
+brew install sass/sass/sass
+```
